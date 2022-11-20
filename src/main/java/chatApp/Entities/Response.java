c430c076039d865bb53610cbc78cd1d6dceaf964
itamarit <itamarit124812@gmail.com>
2022-11-20 11:22:17 +0200

    add response

---
+++ src/main/java/chatApp/Entities/Response.java
@@ -0,0 +1,34 @@
+package chatApp.Entities;
+
+public class Response<T> {
+    private final T data;
+    private final String message;
+    private final boolean isSucceed;
+    private Response(boolean isSucceed,T data,String message)
+    {
+        this.data=data;
+        this.isSucceed=isSucceed;
+        this.message=message;
+    }
+    public static<T> Response createSuccessfulResponse(T data)
+    {
+        return  new Response(true,data,null);
+    }
+    public  static <T> Response createFailureResponse(String message)
+    {
+        return  new Response(false,null,message);
+    }
+    public boolean isSucceed()
+    {
+        return isSucceed;
+    }
+    public T getData()
+    {
+        if(isSucceed) return data;
+        return null;
+    }
+    public String getMessage()
+    {
+        return message;
+    }
+}
