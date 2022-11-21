898231c5606dc6f1ffdb5194755e0be12e34004c
MayaBarkann <mayabarkann@gmail.com>
2022-11-21 19:18:17 +0200

    added userProfileToPresent class

--- src/main/java/chatApp/controller/entities/UserProfileToPresent.java
+++ src/main/java/chatApp/controller/entities/UserProfileToPresent.java
@@ -1,5 +1,8 @@
 package chatApp.controller.entities;
 
+import chatApp.Entities.User;
+import chatApp.Entities.UserProfile;
+
 import java.time.LocalDate;
 
 public class UserProfileToPresent {
@@ -11,5 +14,18 @@ public class UserProfileToPresent {
     private String description;
     private String imageUrl;
 
+    public static UserProfileToPresent createFromUserProfileAndUser(UserProfile userProfile, User user){
+        return new UserProfileToPresent(user.getUsername(),user.getEmail(), userProfile.getFirstName(),
+                userProfile.getLastName(), userProfile.getDateOfBirth(), userProfile.getDescription(), userProfile.getImageUrl());
+    }
 
+    private UserProfileToPresent(String username, String email, String firstName, String lastName, LocalDate dateOfBirth, String description, String imageUrl) {
+        this.username = username;
+        this.email = email;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.dateOfBirth = dateOfBirth;
+        this.description = description;
+        this.imageUrl = imageUrl;
+    }
 }
