97f25d5ff8329879e525aa9d0e4584ffb87c029e
itamarit <itamarit124812@gmail.com>
2022-11-28 10:35:14 +0200

    create authfilter

--- src/main/java/chatApp/filters/LoginFilter.java
+++ src/main/java/chatApp/filters/LoginFilter.java
@@ -1,14 +1,25 @@
 package chatApp.filters;
 
 
+import chatApp.entities.Response;
+import chatApp.service.AuthService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 
 import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 
 public class LoginFilter implements Filter {
+    private final AuthService authService;
+    @Autowired
+    public LoginFilter(AuthService authService) {
+        this.authService = authService;
+    }
+
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
         Filter.super.init(filterConfig);
@@ -16,6 +27,18 @@ public class LoginFilter implements Filter {
 
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest req = (HttpServletRequest) servletRequest;
+        HttpServletResponse res = (HttpServletResponse) servletResponse;
+        String authToken = req.getHeader("Authorization");
+        if (authToken != null) {
+            Response<?> tokenCorrect = authService.isTokenCorrect(authToken);
+            if(!tokenCorrect.isSucceed())
+            {
+
+            }
+        } else {
+            res.sendError(401, "Unauthorized");
+        }
 
     }
 
