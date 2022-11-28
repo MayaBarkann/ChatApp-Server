a1e5a43584fc013837ef8cd9320ba214de017d9a
D-Y-So <68039134+D-Y-So@users.noreply.github.com>
2022-11-28 14:18:11 +0200

    save changes

--- src/main/java/chatApp/SpringApp.java
+++ src/main/java/chatApp/SpringApp.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringApp {
     public static void main(String[] args){
         SpringApplication.run(SpringApp.class, args);
+
     }
 
 }
